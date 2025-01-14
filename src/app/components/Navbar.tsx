ac4ebd7c4620c620677147efc973fd07f9a6b267
hermes7144 <kichancho@gmail.com>
2025-01-14 21:04:23 +0900

    feat:로그인 구현

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,48 @@
+'use client'
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import HomeIcon from './ui/icons/HomeIcon';
+import HomeFillIcon from './ui/icons/HomeFillIcon';
+import SearchIcon from './ui/icons/SearchIcon';
+import SearchFillIcon from './ui/icons/SearchFillIcon';
+import NewIcon from './ui/icons/NewIcon';
+import NewFillIcon from './ui/icons/NewFillIcon';
+import ColorButton from './ui/ColorButton';
+import { useSession, signIn, signOut } from "next-auth/react"
+
+const menu = [
+  {
+    href: '/', icon: <HomeIcon />, clickedIcon: <HomeFillIcon />, 
+  },
+  {
+    href: '/search', icon: <SearchIcon />, clickedIcon: <SearchFillIcon />, 
+  },
+  {
+    href: '/new', icon: <NewIcon />, clickedIcon: <NewFillIcon />, 
+  }
+]
+
+export default function Navbar() {
+  const pathname = usePathname();
+  const { data: session } = useSession();
+
+  return (
+    <header className='flex justify-between items-center px-6'>
+      <h1 className='text-3xl font-bold'>InstantGram</h1>
+      <nav className=''>
+        <ul className='flex items-center gap-4 p-4'>
+        {menu.map(item => (
+          <li key={item.href}>
+            <Link href={item.href}>
+            {item.href === pathname ? item.clickedIcon : item.icon }
+          </Link>
+          </li>
+        ))}
+        {session ? (
+          <ColorButton text='Sign out' onClick={() => signOut()} />
+        ):(<ColorButton text='Sign in' onClick={() => signIn()} />)}
+        </ul>
+      </nav>
+    </header>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
 import './globals.css'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+import AuthContext from './context/AuthContext'
 
 const openSans = Open_Sans({ subsets: ['latin'] })
 
@@ -16,7 +19,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={openSans.className}>{children}</body>
+      
+      <body className={`${openSans.className} w-full max-w-screen-xl overflow-auto mx-auto`}>
+        <AuthContext>
+          <header className='sticky top-0 bg-white z-10 border-b'>
+            <Navbar />
+          </header>
+          <main className='grow'>{children}</main>
+          <Footer/>
+        </AuthContext>
+      </body>
     </html>
   )
 }
